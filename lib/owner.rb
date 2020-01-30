0461e4bc1765a035de4633cc9fc1b0feed7097f5
David Van Fleet <dvfleet413@gmail.com>
2020-01-30 23:56:28 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -16,6 +16,12 @@ class Owner
     Cat.all.select {|cat| cat.owner == self}
   end
   
+  def buy_cat(name)
+    cat = Cat.new(name)
+    cat.owner = self
+    cat
+  end 
+  
   def dogs
     Dog.all.select {|dog| dog.owner == self}
   end
